b316ebeba461310de9278342c5a6da4c42a5ac35
David Laptop <davidnguyenantran@gmail.com>
2023-03-22 12:36:06 -0700

    default values in function

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
-function greet(input) {
-  console.log(`${input}`);
+function greet(input, time = "day") {
+  console.log(`Good ${time} ${input}`);
 }
 
-greet(prompt("elp"));
+greet(time, prompt("elp"));
+
+// let hello = function (input) {
+//   console.log(`Hello ${input}`);
+// };
+// hello(prompt("oh hewwo"));
+
+// let yo = input => console.log(`waddup ${input}`);
+// yo(prompt("wassup cuh"));
