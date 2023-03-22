8a66a483d00c614a0577ea201d820d9289272e49
David Laptop <davidnguyenantran@gmail.com>
2023-03-22 13:43:21 -0700

    query selectors and text content

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-function greet(input, time = "day") {
-  console.log(`Good ${time} ${input}`);
-}
+// function greet(input, time = "day") {
+//   console.log(`Good ${time} ${input}`);
+// }
 
-greet(time, prompt("elp"));
+// greet(prompt("elp"));
 
 // let hello = function (input) {
 //   console.log(`Hello ${input}`);
@@ -11,3 +11,25 @@ greet(time, prompt("elp"));
 
 // let yo = input => console.log(`waddup ${input}`);
 // yo(prompt("wassup cuh"));
+
+function area(radius) {
+  if (isNaN(radius)) {
+    elpPara.textContent += input;
+    mePara.textContent = "This aint a number";
+  } else {
+    let cArea = Math.PI * radius ** 2;
+    return cArea;
+  }
+}
+const elpPara = document.querySelector("#elp");
+console.log(elpPara);
+const mePara = document.querySelector("#me");
+console.log(mePara);
+
+let input = prompt("what is the radius of your circle?");
+let cArea = area(input);
+if (cArea !== undefined) {
+  elpPara.textContent += input;
+  mePara.textContent = cArea;
+  //   alert(`The area of a circle with radis ${input} is ${cArea.toFixed(2)}`);
+}
